0ae353f466b93fc78e90d9a26ab8e33cca5c607f
jonathanzuniga <jonathanzuniga@users.noreply.github.com>
2022-08-26 23:31:12 +0000

    Update Tokens

--- assets/ios-swift/StyleDictionary+Enum.swift
+++ assets/ios-swift/StyleDictionary+Enum.swift
@@ -4,22 +4,22 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 26 Aug 2022 23:30:00 GMT
+// Generated on Fri, 26 Aug 2022 23:31:12 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
     public static let colorsBlack = #000000
-    public static let colorsPrimaryOpacity100 = #22ba1c
-    public static let colorsPrimaryOpacity16 = #22ba1c29
-    public static let colorsPrimaryOpacity40 = #22ba1c66
-    public static let colorsPrimaryOpacity8 = #22ba1c14
-    public static let colorsPrimaryOpacity80 = #22ba1ccc
+    public static let colorsPrimaryOpacity100 = #B91C1C
+    public static let colorsPrimaryOpacity16 = #b91c1c29
+    public static let colorsPrimaryOpacity40 = #b91c1c66
+    public static let colorsPrimaryOpacity8 = #b91c1c14
+    public static let colorsPrimaryOpacity80 = #b91c1ccc
     public static let colorsPurple = #4E04AF
     public static let colorsRed300 = #FCA5A5
     public static let colorsRed500 = #EF4444
-    public static let colorsRed700 = #22ba1c
+    public static let colorsRed700 = #B91C1C
     public static let colorsWhite = #ffffff
     public static let image = 'x.jpg'
     public static let spacingLg = 16
--- assets/ios-swift/StyleDictionary+Struct.swift
+++ assets/ios-swift/StyleDictionary+Struct.swift
@@ -4,22 +4,22 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 26 Aug 2022 23:30:00 GMT
+// Generated on Fri, 26 Aug 2022 23:31:12 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
     internal static let colorsBlack = #000000
-    internal static let colorsPrimaryOpacity100 = #22ba1c
-    internal static let colorsPrimaryOpacity16 = #22ba1c29
-    internal static let colorsPrimaryOpacity40 = #22ba1c66
-    internal static let colorsPrimaryOpacity8 = #22ba1c14
-    internal static let colorsPrimaryOpacity80 = #22ba1ccc
+    internal static let colorsPrimaryOpacity100 = #B91C1C
+    internal static let colorsPrimaryOpacity16 = #b91c1c29
+    internal static let colorsPrimaryOpacity40 = #b91c1c66
+    internal static let colorsPrimaryOpacity8 = #b91c1c14
+    internal static let colorsPrimaryOpacity80 = #b91c1ccc
     internal static let colorsPurple = #4E04AF
     internal static let colorsRed300 = #FCA5A5
     internal static let colorsRed500 = #EF4444
-    internal static let colorsRed700 = #22ba1c
+    internal static let colorsRed700 = #B91C1C
     internal static let colorsWhite = #ffffff
     internal static let image = 'x.jpg'
     internal static let spacingLg = 16
