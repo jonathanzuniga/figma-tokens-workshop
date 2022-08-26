86a3dc534e76420450019084971d0a072b837c56
jonathanzuniga <jonathanzuniga@users.noreply.github.com>
2022-08-26 00:21:04 +0000

    Update Tokens

---
+++ assets/ios-swift/StyleDictionary+Enum.swift
@@ -0,0 +1,32 @@
+
+//
+// StyleDictionary+Enum.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 26 Aug 2022 00:21:02 GMT
+
+
+import UIKit
+
+public enum StyleDictionaryEnum {
+    public static let colorsBlack = #000000
+    public static let colorsPrimaryOpacity100 = #B91C1C
+    public static let colorsPrimaryOpacity16 = #b91c1c29
+    public static let colorsPrimaryOpacity40 = #b91c1c66
+    public static let colorsPrimaryOpacity8 = #b91c1c14
+    public static let colorsPrimaryOpacity80 = #b91c1ccc
+    public static let colorsPurple = #4E04AF
+    public static let colorsRed300 = #FCA5A5
+    public static let colorsRed500 = #EF4444
+    public static let colorsRed700 = #B91C1C
+    public static let colorsWhite = #ffffff
+    public static let image = 'x.jpg'
+    public static let spacingLg = 16
+    public static let spacingMd = 8
+    public static let spacingSm = 4
+    public static let spacingXl = 32
+    public static let tokenSetOrder0 = global
+    public static let tokenSetOrder1 = other set
+    public static let xxl = 64
+}
---
+++ assets/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,32 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 26 Aug 2022 00:21:02 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let colorsBlack = #000000
+    internal static let colorsPrimaryOpacity100 = #B91C1C
+    internal static let colorsPrimaryOpacity16 = #b91c1c29
+    internal static let colorsPrimaryOpacity40 = #b91c1c66
+    internal static let colorsPrimaryOpacity8 = #b91c1c14
+    internal static let colorsPrimaryOpacity80 = #b91c1ccc
+    internal static let colorsPurple = #4E04AF
+    internal static let colorsRed300 = #FCA5A5
+    internal static let colorsRed500 = #EF4444
+    internal static let colorsRed700 = #B91C1C
+    internal static let colorsWhite = #ffffff
+    internal static let image = 'x.jpg'
+    internal static let spacingLg = 16
+    internal static let spacingMd = 8
+    internal static let spacingSm = 4
+    internal static let spacingXl = 32
+    internal static let tokenSetOrder0 = global
+    internal static let tokenSetOrder1 = other set
+    internal static let xxl = 64
+}
