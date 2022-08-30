0868de7d20b83371771bca4b8dd929587a3bdae7
jonathanzuniga <jonathanzuniga@users.noreply.github.com>
2022-08-30 19:07:14 +0000

    Update Tokens

--- assets/ios-swift/StyleDictionary+Enum.swift
+++ assets/ios-swift/StyleDictionary+Enum.swift
@@ -4,30 +4,87 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 29 Aug 2022 17:19:23 GMT
+// Generated on Tue, 30 Aug 2022 19:07:14 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
+    public static let borderRadius4 = 4
     public static let colorsBlack = #000000
-    public static let colorsLavenderFloral = #b591f9
-    public static let colorsPrimaryOpacity100 = #B91C1C
-    public static let colorsPrimaryOpacity16 = #b91c1c29
-    public static let colorsPrimaryOpacity40 = #b91c1c66
-    public static let colorsPrimaryOpacity8 = #b91c1c14
-    public static let colorsPrimaryOpacity80 = #b91c1ccc
-    public static let colorsPurple = #4E04AF
-    public static let colorsRed300 = #FCA5A5
-    public static let colorsRed500 = #EF4444
-    public static let colorsRed700 = #B91C1C
-    public static let colorsWhite = #ffffff
-    public static let image = 'x.jpg'
-    public static let spacingLg = 16
-    public static let spacingMd = 8
-    public static let spacingSm = 4
-    public static let spacingXl = 32
+    public static let colorsGreen100 = #58FF9D
+    public static let colorsGreen16 = #E4FFEF
+    public static let colorsGreen40 = #BCFFD8
+    public static let colorsGreen8 = #F2FFF7
+    public static let colorsGreen80 = #79FFB1
+    public static let colorsLight100 = #CCD2DD
+    public static let colorsLight16 = #F7F8FA
+    public static let colorsLight40 = #EBEDF1
+    public static let colorsLight8 = #FBFBFC
+    public static let colorsLight80 = #D6DBE4
+    public static let colorsPurple100 = #4E04AF
+    public static let colorsPurple16 = #E3D7F2
+    public static let colorsPurple40 = #B89BDF
+    public static let colorsPurple8 = #F1EBF9
+    public static let colorsPurple80 = #7136BF
+    public static let colorsSlate100 = #757D88
+    public static let colorsSlate16 = #E9EAEC
+    public static let colorsSlate40 = #C8CBCF
+    public static let colorsSlate8 = #F4F5F5
+    public static let colorsSlate80 = #9197A0
+    public static let colorsSpace100 = #333366
+    public static let colorsSpace16 = #DEDEE7
+    public static let colorsSpace40 = #ADADC2
+    public static let colorsSpace8 = #EFEFF3
+    public static let colorsSpace80 = #5C5C85
+    public static let colorsWhite = #FFFFFF
+    public static let fontFamily = Poppins
+    public static let fontSizeBody = 16
+    public static let fontSizeH1 = 32
+    public static let fontSizeH2 = 24
+    public static let fontSizeH3 = 18
+    public static let fontSizeMedium = 14
+    public static let fontSizeSmall = 12
+    public static let fontWeightRegular = Regular
+    public static let fontWeightSemiBold = SemiBold
+    public static let headingsH1 = [object Object]
+    public static let headingsH2 = [object Object]
+    public static let headingsH3 = [object Object]
+    public static let inputActiveBorder = #B89BDF
+    public static let inputActiveForeground = #333366
+    public static let inputDefaultBackground = #FFFFFF
+    public static let inputDefaultBorder = #CCD2DD
+    public static let inputDefaultForeground = #757D88
+    public static let inputFieldDescription = #757D88
+    public static let inputFieldLabel = #333366
+    public static let lineHeightAuto = AUTO
+    public static let multitaskr = [object Object]
+    public static let semanticBgCanvas = #4E04AF
+    public static let semanticBgDefault = #FFFFFF
+    public static let semanticCtaDefault = linear-gradient(90deg, #4E04AF 0%, #7136BF 100%)
+    public static let semanticCtaDisabled = #CCD2DD
+    public static let semanticCtaMuted = #B89BDF
+    public static let semanticFgAccent = #7136BF
+    public static let semanticFgDefault = #333366
+    public static let semanticFgOnAccent = #FFFFFF
+    public static let semanticFgSubtle = #757D88
+    public static let semanticPrimaryDefault = #4E04AF
+    public static let spacing0 = 0
+    public static let spacing1 = 8
+    public static let spacing10 = 80
+    public static let spacing2 = 16
+    public static let spacing3 = 24
+    public static let spacing4 = 32
+    public static let spacing5 = 40
+    public static let spacing6 = 48
+    public static let spacing7 = 56
+    public static let spacing8 = 64
+    public static let spacing9 = 72
+    public static let textsRegularBody = [object Object]
+    public static let textsRegularMedium = [object Object]
+    public static let textsRegularSmall = [object Object]
+    public static let textsSemiBoldBody = [object Object]
+    public static let textsSemiBoldMedium = [object Object]
     public static let tokenSetOrder0 = global
-    public static let tokenSetOrder1 = other set
-    public static let xxl = 64
+    public static let tokenSetOrder1 = semantic
 }
--- assets/ios-swift/StyleDictionary+Struct.swift
+++ assets/ios-swift/StyleDictionary+Struct.swift
@@ -4,30 +4,87 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 29 Aug 2022 17:19:23 GMT
+// Generated on Tue, 30 Aug 2022 19:07:14 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
+    internal static let borderRadius4 = 4
     internal static let colorsBlack = #000000
-    internal static let colorsLavenderFloral = #b591f9
-    internal static let colorsPrimaryOpacity100 = #B91C1C
-    internal static let colorsPrimaryOpacity16 = #b91c1c29
-    internal static let colorsPrimaryOpacity40 = #b91c1c66
-    internal static let colorsPrimaryOpacity8 = #b91c1c14
-    internal static let colorsPrimaryOpacity80 = #b91c1ccc
-    internal static let colorsPurple = #4E04AF
-    internal static let colorsRed300 = #FCA5A5
-    internal static let colorsRed500 = #EF4444
-    internal static let colorsRed700 = #B91C1C
-    internal static let colorsWhite = #ffffff
-    internal static let image = 'x.jpg'
-    internal static let spacingLg = 16
-    internal static let spacingMd = 8
-    internal static let spacingSm = 4
-    internal static let spacingXl = 32
+    internal static let colorsGreen100 = #58FF9D
+    internal static let colorsGreen16 = #E4FFEF
+    internal static let colorsGreen40 = #BCFFD8
+    internal static let colorsGreen8 = #F2FFF7
+    internal static let colorsGreen80 = #79FFB1
+    internal static let colorsLight100 = #CCD2DD
+    internal static let colorsLight16 = #F7F8FA
+    internal static let colorsLight40 = #EBEDF1
+    internal static let colorsLight8 = #FBFBFC
+    internal static let colorsLight80 = #D6DBE4
+    internal static let colorsPurple100 = #4E04AF
+    internal static let colorsPurple16 = #E3D7F2
+    internal static let colorsPurple40 = #B89BDF
+    internal static let colorsPurple8 = #F1EBF9
+    internal static let colorsPurple80 = #7136BF
+    internal static let colorsSlate100 = #757D88
+    internal static let colorsSlate16 = #E9EAEC
+    internal static let colorsSlate40 = #C8CBCF
+    internal static let colorsSlate8 = #F4F5F5
+    internal static let colorsSlate80 = #9197A0
+    internal static let colorsSpace100 = #333366
+    internal static let colorsSpace16 = #DEDEE7
+    internal static let colorsSpace40 = #ADADC2
+    internal static let colorsSpace8 = #EFEFF3
+    internal static let colorsSpace80 = #5C5C85
+    internal static let colorsWhite = #FFFFFF
+    internal static let fontFamily = Poppins
+    internal static let fontSizeBody = 16
+    internal static let fontSizeH1 = 32
+    internal static let fontSizeH2 = 24
+    internal static let fontSizeH3 = 18
+    internal static let fontSizeMedium = 14
+    internal static let fontSizeSmall = 12
+    internal static let fontWeightRegular = Regular
+    internal static let fontWeightSemiBold = SemiBold
+    internal static let headingsH1 = [object Object]
+    internal static let headingsH2 = [object Object]
+    internal static let headingsH3 = [object Object]
+    internal static let inputActiveBorder = #B89BDF
+    internal static let inputActiveForeground = #333366
+    internal static let inputDefaultBackground = #FFFFFF
+    internal static let inputDefaultBorder = #CCD2DD
+    internal static let inputDefaultForeground = #757D88
+    internal static let inputFieldDescription = #757D88
+    internal static let inputFieldLabel = #333366
+    internal static let lineHeightAuto = AUTO
+    internal static let multitaskr = [object Object]
+    internal static let semanticBgCanvas = #4E04AF
+    internal static let semanticBgDefault = #FFFFFF
+    internal static let semanticCtaDefault = linear-gradient(90deg, #4E04AF 0%, #7136BF 100%)
+    internal static let semanticCtaDisabled = #CCD2DD
+    internal static let semanticCtaMuted = #B89BDF
+    internal static let semanticFgAccent = #7136BF
+    internal static let semanticFgDefault = #333366
+    internal static let semanticFgOnAccent = #FFFFFF
+    internal static let semanticFgSubtle = #757D88
+    internal static let semanticPrimaryDefault = #4E04AF
+    internal static let spacing0 = 0
+    internal static let spacing1 = 8
+    internal static let spacing10 = 80
+    internal static let spacing2 = 16
+    internal static let spacing3 = 24
+    internal static let spacing4 = 32
+    internal static let spacing5 = 40
+    internal static let spacing6 = 48
+    internal static let spacing7 = 56
+    internal static let spacing8 = 64
+    internal static let spacing9 = 72
+    internal static let textsRegularBody = [object Object]
+    internal static let textsRegularMedium = [object Object]
+    internal static let textsRegularSmall = [object Object]
+    internal static let textsSemiBoldBody = [object Object]
+    internal static let textsSemiBoldMedium = [object Object]
     internal static let tokenSetOrder0 = global
-    internal static let tokenSetOrder1 = other set
-    internal static let xxl = 64
+    internal static let tokenSetOrder1 = semantic
 }
